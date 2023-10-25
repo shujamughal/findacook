d1d2983f9916ef3f1c0d08180cbb87e2d5c726a3
razib <coolrazi110@gmail.com>
2023-10-25 11:08:41 +0500

    initial commit

---
+++ FindaCook/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FindaCook
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
